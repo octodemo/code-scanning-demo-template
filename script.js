3935c4e3512299a58aba1121c1b171f4c0c6757b
Justin Hutchings <jhutchings1@users.noreply.github.com>
2020-05-08 09:18:01 -0700

    Add API key

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const apiKey = "abcdefghijklmnopqrstuvwxyz234567abcdefghijklmnopqrst";
+
 function endsWith(x, y) {
   let index = x.lastIndexOf(y);
   return index !== -1 && index === x.length - y.length;
