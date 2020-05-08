dfd63aef508065b536307fba81904c768a6767d7
Justin Hutchings <jhutchings1@users.noreply.github.com>
2020-05-08 09:21:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const apiKey = "abcdefghijklmnopqrstuvwxyz234567abcdefghijklmnopqrst";
+const apiKey2 = "12345678901234567890123456789012-12345678-12345678";
 
 function endsWith(x, y) {
   let index = x.lastIndexOf(y);
