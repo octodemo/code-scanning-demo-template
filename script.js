0fcdc4d31dfd8ce51e737dd4711e4ed893c2ade3
Lindsey Bocatto <97491505+lindseybocatto@users.noreply.github.com>
2022-07-29 09:57:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function endsWith(x, y) {
   let index = x.lastIndexOf(y);
   return index !== -1 && index === x.length - y.length;
 }
+
+function endsWith(x, y) {
+  return x.lastIndexOf(y) === x.length - y.length;
+}
